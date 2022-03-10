4a9dbd2172857e9d3fa70bedc25134730d742755
Jalen-Allison <jalenallison21@yahoo.com>
2022-03-10 09:46:25 -0800

    math operators

--- Basic JavaScript Projects/Project2_functions/JS/main2.js
+++ Basic JavaScript Projects/Project2_functions/JS/main2.js
@@ -12,6 +12,7 @@ function myFunction() { //Definded and named a function
     var sentence = "I am learning"; //Defining the sentence as a variable
     sentence += " a lot from this course!"; //concatenating "I am learning" & "a lot from this course" making it a sentence 
     document.getElementById("Concatenate").innerHTML = sentence; //Links the "Click here!" to the sentence above and displays the sentence when clicked.
+}
 
 function My_First_Function() { //Defining a function and naming it
     var str = "This is the button text!"; //Defining a variable and giving it a string value
--- Basic JavaScript Projects/Project3_math_operators/JS/main3.js
+++ Basic JavaScript Projects/Project3_math_operators/JS/main3.js
@@ -1,4 +1,52 @@
-function addition_Function() {
+function addition_Function() {  //Addition code
     var addition = 2 + 2;
-    document.getElementById("Math").innerHTML = "2 +2 = " + addition;
+    document.getElementById("Math").innerHTML = "2 +2 = " + addition; 
 }
+
+function subtraction_Function() { //subtraction code
+    var subtraction = 5 - 2;
+    document.getElementById("Math").innerHTML = "5 -2 = " + subtraction;
+}
+
+function multiplication_Function() { //Multiplication code
+    var simple_Math = 6 * 2;
+    document.getElementById("Math").innerHTML = "6 x 2 = " + simple_Math;
+}
+
+function division_Function() { //division code
+    var simple_Math = 48 / 6;
+    document.getElementById("Math").innerHTML = "48 / 6 = " + simple_Math;
+}
+
+function more_Math() { //multiple operations code
+    var simple_Math = (1+2) * 10 / 2 -5;
+    document.getElementById("Math").innerHTML = "1 plus 2, multiplied by 10, divided in half and then subtracted by 5 equals " + simple_Math;
+}
+
+function modulus_Operator() { //getting a remainder code
+    var simple_Math = 25 % 6;
+    document.getElementById("Math").innerHTML = "When you divide 25 by 6 you have a remainder of: " + simple_Math;
+}
+
+function negation_Operator() { //getting the opposite integer of x
+    var x = 10; 
+    document.getElementById("Math").innerHTML = -x;
+}
+
+var X = 5;  //Increment increase value by one
+X++;
+document.write(X); 
+
+var X = 5.25; //Decrement decrease value by one
+X--;
+document.write(X);
+
+window.alert(Math.random()); //pop up when you open webpage
+
+
+
+
+
+
+
+
