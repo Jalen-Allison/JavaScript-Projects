6db28cc7daf9c056fea6f38a274ad721fa41bd5f
Jalen-Allison <jalenallison21@yahoo.com>
2022-03-22 09:06:41 -0700

    countdown timer/slideshow

--- Basic JavaScript Projects/Project9_countdown_slideshow/JS/main9.js
+++ Basic JavaScript Projects/Project9_countdown_slideshow/JS/main9.js
@@ -1,3 +1,20 @@
+function countdown() {
+    var seconds = document.getElementById("seconds").value;
+
+    function tick() {
+        seconds = seconds -1;
+        timer.innerHTML = seconds;
+        var time = setTimeout(tick, 1000);
+        if (seconds == -1) {
+            alert("Time's up!");
+            clearTimeout(time);
+            timer.innerHTML = "";
+        }
+    }
+    tick();
+}
+
+
 var slideIndex = 1;
 showSlides(slideIndex);
 
@@ -17,10 +34,10 @@ function showSlides(n) {
     var dots = document.getElementsByClassName("dot");
     if (n > slides.length) {slideIndex = 1}
     if (n < 1) {slideIndex = slides.length}
-    for (i = 0; o < slides.length; i++) {
+    for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    for (i = 0; i < dots.length; i++) {
+    for (i = 0; i < slides.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
     slides[slideIndex-1].style.display = "block";
