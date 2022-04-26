0261d4954788ebef460468cf41243947910cc092
Jalen-Allison <jalenallison21@yahoo.com>
2022-04-26 10:56:28 -0700

    Start of switch statements

---
+++ Basic JavaScript Projects/JavaScript projects/JavaScript/JavaScript_1.js
@@ -0,0 +1,17 @@
+function Color_Function() {
+    var Color_Output;
+    var Colors = document.getElementById("Color_Input").ariaValueMax;
+    var Color_String = " is a great color!";
+    switch(Colors) {
+        case "Red":
+            Color_Output = "Red" + Color_String
+        break;
+        case "Yellow":
+        case "Red":
+            Color_Output = "Red" + Color_String
+            break;
+            case "Yellow":
+                
+
+
+}
