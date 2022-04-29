85a0a0b5b924719eeee9a8d285cfd4bc14518a24
Jalen Allison <jalenallison21@yahoo.com>
2022-04-29 11:46:36 -0700

    Contact form, gradient color, shape, getelementclassname, canvass,

--- Basic JavaScript Projects/JavaScript projects/JavaScript/JavaScript_1.js
+++ Basic JavaScript Projects/JavaScript projects/JavaScript/JavaScript_1.js
@@ -1,17 +1,51 @@
+// Switch statements - performs different actions based on the conditions presented to it. It is a type of conditional statement.
 function Color_Function() {
     var Color_Output;
-    var Colors = document.getElementById("Color_Input").ariaValueMax;
+    var Colors = document.getElementById("Color_Input").value;
     var Color_String = " is a great color!";
     switch(Colors) {
         case "Red":
-            Color_Output = "Red" + Color_String
-        break;
-        case "Yellow":
-        case "Red":
-            Color_Output = "Red" + Color_String
+            Color_Output = "Red" + Color_String;
             break;
-            case "Yellow":
-                
-
-
+        case "Yellow":
+            Color_Output = "Yellow" + Color_String;
+            break;
+        case "Green":
+            Color_Output = "Green" + Color_String;
+            break;
+        case "Blue":
+            Color_Output = "Blue" + Color_String;
+            break;
+        case "Pink":
+            Color_Output = "Pink" + Color_String;
+            break;
+        case "Purple":
+            Color_Output = "Purple" + Color_String;
+            break;
+            default:
+            Color_Output = "Please enter a color exactly as written on the above list.";
+    }
+            document.getElementById("Output").innerHTML = Color_Output;
 }
+
+function Hello_World_Function() {
+    var A = document.getElementsByClassName("Click");
+    A[0].innerHTML = "The text has changed!";
+}
+
+// The HTML <canvas> element is used to draw graphics, on the fly, via JavaScript.
+var c = document.getElementById("Border");
+var ctx = c.getContext("2d");
+ctx.moveTo(0,0);
+ctx.lineTo(200,100);
+ctx.stroke();
+
+//The createLinearGradient() method creates a linear gradient object.
+var c = document.getElementById("Border");
+var ctx = c.getContext("2d");
+var grd = ctx.createLinearGradient(0, 0, 170, 0);
+grd.addColorStop(0, "black");
+grd.addColorStop(1, "white");
+ctx.fillStyle = grd;
+ctx.fillRect(20, 20, 150, 100);
+
